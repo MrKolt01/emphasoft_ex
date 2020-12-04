6df8c17edc4809d7c29c66b6cac82fb3aa5b43b0
BRAINSYSTEMS/L.Karavashkin <lev.karavashkin@brainsystems.ru>
2020-12-04 18:18:02 +0300

    Api endpoint update

--- src/api/config/ApiConnect.js
+++ src/api/config/ApiConnect.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ENDPOINT = 'http://emphasoft-test-assignment.herokuapp.com'
+const API_ENDPOINT = 'https://emphasoft-test-assignment.herokuapp.com'
 
 export const ApiConnect = axios.create({
   baseURL: API_ENDPOINT,
