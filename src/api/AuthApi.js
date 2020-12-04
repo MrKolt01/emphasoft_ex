5f948dcd28cf1d4ea2a452503c3dd477ec788419
BRAINSYSTEMS/L.Karavashkin <lev.karavashkin@brainsystems.ru>
2020-12-04 18:04:33 +0300

    cors

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -2,6 +2,8 @@ import { ApiConnect } from './config/ApiConnect'
 
 export const AuthApi = {
   auth(user) {
-    return ApiConnect.post('/api-token-auth/', user)
+    return ApiConnect.post('/api-token-auth/', user, {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+    })
   },
 }
--- src/api/UsersApi.js
+++ src/api/UsersApi.js
@@ -4,6 +4,7 @@ export const UsersApi = {
   getUsers(token) {
     return ApiConnect.get('/api/v1/users/', {
       headers: {
+        'Access-Control-Allow-Origin': '*',
         Authorization: `Token ${token}`,
       },
     })
