56b3efe24f09b1387db67db1db95e73b4b8c4483
mrkolt01 <kolt.zar@mail.ru>
2020-12-07 14:34:05 +0300

    cors

--- src/api/UsersApi.js
+++ src/api/UsersApi.js
@@ -8,7 +8,7 @@ export const UsersApi = {
         'Access-Control-Allow-Credentials': true,
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
         'Access-Control-Allow-Headers':
-          'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+          'Access-Control-Allow-Headers,Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Access-Control-Request-Method,Access-Control-Request-Headers',
         Authorization: `Token ${token}`,
       },
     })
