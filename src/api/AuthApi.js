636014c6081bcdeba333702ba9897f632cfaf829
mrkolt01 <kolt.zar@mail.ru>
2020-12-07 14:49:22 +0300

    cors

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -2,14 +2,6 @@ import { ApiConnect } from './config/ApiConnect'
 
 export const AuthApi = {
   auth(user) {
-    return ApiConnect.post('/api-token-auth/', user, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-        'Access-Control-Allow-Headers':
-          'Access-Control-Allow-Headers,Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Access-Control-Request-Method,Access-Control-Request-Headers',
-      },
-    })
+    return ApiConnect.post('/api-token-auth/', user,)
   },
 }
--- src/api/UsersApi.js
+++ src/api/UsersApi.js
@@ -4,11 +4,6 @@ export const UsersApi = {
   getUsers(token) {
     return ApiConnect.get('/api/v1/users/', {
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-        'Access-Control-Allow-Headers':
-          'Access-Control-Allow-Headers,Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Access-Control-Request-Method,Access-Control-Request-Headers',
         Authorization: `Token ${token}`,
       },
     })
